009c30bcf0d91707169c48ee1bec87b85fe2af71
Jesse Lind <Jesseclind@gmail.com>
2021-10-26 12:00:23 -0400

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import './Footer.css'
+import logo from '../../assets/images/ld-logo-black.png'
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <img src={logo} alt='Lind Designs' />
+      <p className='copyright'>
+        © Copyright {new Date().getFullYear()} Lind Designs
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
