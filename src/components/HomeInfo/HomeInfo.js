2a3e078fa065d07d618ceb6faec4638acec9cff9
Jesse Lind <Jesseclind@gmail.com>
2021-10-26 11:49:58 -0400

    Add home info section for information on lind designs

---
+++ src/components/HomeInfo/HomeInfo.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import './HomeInfo.css'
+import {
+  BsPeople,
+  BsBagCheck,
+  BsGear,
+  BsChatText,
+  BsChat,
+} from 'react-icons/bs'
+const HomeInfo = () => {
+  return (
+    <section className='home-info section'>
+      <div className='section-inner'>
+        <h1 className='section-title'>Welcome to Lind Designs</h1>
+        <div className='grid'>
+          <article className='about'>
+            <div className='icon-container'>
+              <BsPeople className='icon' />
+            </div>
+            <h2 className='article-title'>About Us</h2>
+            <p className='article-text'>
+              Lind Designs is a provider of enterprise technology solutions and
+              services, with expertise enabling organizations to overcome
+              obstacles and achieve excellent business results. We help
+              companies streamline and improve the way they buy, implement and
+              manage their technology.
+            </p>
+          </article>
+          <article className='products'>
+            <div className='icon-container'>
+              <BsBagCheck className='icon' />
+            </div>
+            <h2 className='article-title'>Products</h2>
+            <p className='article-text'>
+              Leading vendor partners:
+              <ul>
+                <li>
+                  <a
+                    href='https://www.hpe.com/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    HPE
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://www.microsoft.com/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Microsoft
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://www.vmware.com/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    VMware
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='https://www.veeam.com/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Veeam
+                  </a>
+                </li>
+              </ul>
+            </p>
+          </article>
+          <article className='services'>
+            <div className='icon-container'>
+              <BsGear className='icon' />
+            </div>
+            <h2 className='article-title'>Services</h2>
+            <p className='article-text'>
+              We specialize in virtualization and backup services tailored to
+              your servicing needs.
+            </p>
+          </article>
+          <article className='contact'>
+            <div className='icon-container'>
+              <BsChatText className='icon' />
+            </div>
+            <h2 className='article-title'>Contact us</h2>
+            <p className='article-text'>
+              Email dlind@linddesigns for a free consultation.
+            </p>
+            <button className='page-btn'>Contact</button>
+          </article>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HomeInfo
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import HomeHero from '../../components/HomeHero/HomeHero'
+import HomeInfo from '../../components/HomeInfo/HomeInfo'
+
 const Home = () => {
   return (
     <div className='page home-page'>
       <HomeHero />
+      <HomeInfo />
     </div>
   )
 }
